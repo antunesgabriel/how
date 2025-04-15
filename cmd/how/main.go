9c3c75e4a37d73ccdadc8e9c81ff4381c6bba013
Antunes Gabriel <gabrielantunescontato@gmail.com>
2025-04-15 19:55:44 -0300

    setup chat

--- cmd/how/main.go
+++ cmd/how/main.go
@@ -4,90 +4,17 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/antunesgabriel/how-ai/presetation"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/glamour"
-	"github.com/charmbracelet/lipgloss"
 )
-
-type errMsg error
-
-type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
-}
-
-var quitKeys = key.NewBinding(
-	key.WithKeys("q", "esc", "ctrl+c"),
-	key.WithHelp("", "press q to quit"),
-)
-
-func initialModel() model {
-	s := spinner.New()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
-}
-
-func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	case tea.KeyMsg:
-		if key.Matches(msg, quitKeys) {
-			m.quitting = true
-			return m, tea.Quit
-
-		}
-		return m, nil
-	case errMsg:
-		m.err = msg
-		return m, nil
-
-	default:
-		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
-		return m, cmd
-	}
-}
-
-func (m model) View() string {
-	if m.err != nil {
-		return m.err.Error()
-	}
-
-	in := `# Hello World
-
-	This is a simple example of Markdown rendering with Glamour!
-	Check out the [other examples](https://github.com/charmbracelet/glamour/tree/master/examples) too.
-
-	Bye!
-`
-
-	out, err := glamour.Render(in, "dark")
-	if err != nil {
-		return m.err.Error()
-	}
-
-	str := fmt.Sprintf(
-		"%s\n\n   %s Loading forever... %s\n\n",
-		out,
-		m.spinner.View(),
-		quitKeys.Help().Desc,
-	)
-	if m.quitting {
-		return str + "\n"
-	}
-	return str
-}
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	chat, err := presetation.NewChat()
+	if err != nil {
+		panic(err)
+	}
+
+	p := tea.NewProgram(chat)
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
---
+++ presetation/chat.go
@@ -0,0 +1,168 @@
+package presetation
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type (
+	errMsg error
+)
+
+var quitKeys = key.NewBinding(
+	key.WithKeys("esc", "ctrl+c"),
+	key.WithHelp("", "press esc or ctrl+c to quit"),
+)
+
+const gap = "\n\n"
+
+type Chat struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+	glam        *glamour.TermRenderer
+	spinner     spinner.Model
+	quitting    bool
+}
+
+const llmMockContent = `
+# Today’s Menu
+
+## Appetizers
+
+| Name        | Price | Notes                           |
+| ---         | ---   | ---                             |
+| Tsukemono   | $2    | Just an appetizer               |
+| Tomato Soup | $4    | Made with San Marzano tomatoes  |
+| Okonomiyaki | $4    | Takes a few minutes to make     |
+| Curry       | $3    | We can add squash if you’d like |
+
+## Seasonal Dishes
+
+| Name                 | Price | Notes              |
+| ---                  | ---   | ---                |
+| Steamed bitter melon | $2    | Not so bitter      |
+| Takoyaki             | $3    | Fun to eat         |
+| Winter squash        | $3    | Today it's pumpkin |
+
+## Desserts
+
+| Name         | Price | Notes                 |
+| ---          | ---   | ---                   |
+| Dorayaki     | $4    | Looks good on rabbits |
+| Banana Split | $5    | A classic             |
+| Cream Puff   | $3    | Pretty creamy!        |
+
+All our dishes are made in-house by Karen, our chef. Most of our ingredients
+are from our garden or the fish market down the street.
+
+Some famous people that have eaten here lately:
+
+* [x] René Redzepi
+* [x] David Chang
+* [ ] Jiro Ono (maybe some day)
+
+Bon appétit!
+`
+
+func NewChat() (*Chat, error) {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 280
+
+	ta.SetWidth(30)
+	ta.SetHeight(3)
+
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!\nType a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	glam, err := glamour.NewTermRenderer(glamour.WithEnvironmentConfig(), glamour.WithWordWrap(80))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Chat{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+		glam:        glam,
+	}, nil
+}
+
+func (m Chat) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m Chat) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		m.textarea.SetWidth(msg.Width)
+		m.viewport.Height = msg.Height - m.textarea.Height() - lipgloss.Height(gap)
+
+		if len(m.messages) > 0 {
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+		}
+		m.viewport.GotoBottom()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m Chat) View() string {
+	// out, err := m.glam.Render(llmMockContent)
+	// if err != nil {
+	// 	return m.err.Error()
+	// }
+
+	return fmt.Sprintf(
+		"%s%s%s\n%s",
+		m.viewport.View(),
+		gap,
+		m.textarea.View(),
+		quitKeys.Help().Desc,
+	)
+}
