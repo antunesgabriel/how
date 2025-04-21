644facb2c5b4a59e9383b8b1f859db71ff31e7ab
Antunes Gabriel <gabrielantunescontato@gmail.com>
2025-04-21 17:26:47 -0300

    wip: agent shell

--- infrastructure/orchestration/agent/agent.go
+++ infrastructure/orchestration/agent/agent.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"context"
+	"strings"
 
 	"github.com/cloudwego/eino-ext/components/tool/duckduckgo"
 	einomodel "github.com/cloudwego/eino/components/model"
@@ -18,7 +19,7 @@ func (a *Agent) GetResponse(ctx context.Context, input string) (string, error) {
 	messages := []*schema.Message{
 		{
 			Role:    schema.System,
-			Content: "You are an expert in shell commands and terminal operations. Your task is to provide detailed, accurate explanations of shell commands that users are considering executing. Break down each part of the command, explain what it does, identify any potential risks or side effects, and explain why someone might want to run it. Be specific about what files or systems will be affected. If the command could potentially be harmful, make sure to clearly highlight those risks.",
+			Content: "You are an expert in shell commands and terminal operations. Your task is search and to provide detailed, accurate explanations of shell commands that users are considering executing. Break down each part of the command, explain what it does, identify any potential risks or side effects, and explain why someone might want to run it. Be specific about what files or systems will be affected. If the command could potentially be harmful, make sure to clearly highlight those risks.",
 		},
 		{
 			Role:    schema.User,
@@ -27,21 +28,15 @@ func (a *Agent) GetResponse(ctx context.Context, input string) (string, error) {
 	}
 
 	resp, err := a.runnable.Invoke(ctx, messages)
-	if err != nil {
-		return "I'm sorry, I couldn't generate a response.", err
+	if err != nil && !strings.Contains(err.Error(), "no tool call found in input message") {
+		return "Error on generate response", err
 	}
 
 	if len(resp) == 0 {
 		return "I'm sorry, I couldn't generate a response.", nil
 	}
 
-	content := ""
-
-	for _, message := range resp {
-		content += message.Content
-	}
-
-	return content, nil
+	return resp[0].Content, nil
 }
 
 func NewAgent(ctx context.Context, mc einomodel.ToolCallingChatModel) (*Agent, error) {
@@ -71,8 +66,8 @@ func NewAgent(ctx context.Context, mc einomodel.ToolCallingChatModel) (*Agent, e
 	})
 
 	chain := compose.NewChain[[]*schema.Message, []*schema.Message]()
-	chain.AppendChatModel(mcWithTools, compose.WithNodeName("chat_model"))
-	chain.AppendToolsNode(toolsNode, compose.WithNodeName("tools"))
+	chain.AppendChatModel(mcWithTools, compose.WithNodeName("chat_model")).
+		AppendToolsNode(toolsNode, compose.WithNodeName("tools"))
 
 	runnable, err := chain.Compile(ctx)
 	if err != nil {
